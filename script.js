d2abc8561352d0eeba315fd9c281970704d989c8
RobinsonResearchInstitute <61482118+RobinsonResearchInstitute@users.noreply.github.com>
2022-06-14 18:19:47 +1200

    Update script.js

--- script.js
+++ script.js
@@ -58,6 +58,31 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 } 
 
+function toggle(){
+    document.body.classList.toggle('dark');
+
+    // Let's say the theme is equal to light
+  let theme = "light";
+  // If the body contains the .dark-theme class...
+  if (document.body.classList.contains("dark")) {
+    // ...then let's make the theme dark
+    theme = "dark";
+  }
+  // Then save the choice in localStorage
+  localStorage.setItem("theme", theme);
+
+}
+
+
+// Select the theme preference from localStorage
+const currentTheme = localStorage.getItem("theme");
+document.getElementById("darkSwitch").checked = false;
+// If the current theme in localStorage is "dark"...
+if (currentTheme == "dark") {
+  // ...then use the .dark-theme class
+  document.body.classList.add("dark");
+  document.getElementById("darkSwitch").checked = true;
+}
 
 var newScript = document.createElement("script");
         newScript.src = "https://badge.dimensions.ai/badge.js";
